b57485513e7072d369c28728a2a110f77d00ffd1
qiamaiwa <liliqin@bupt.edu.cn>
2022-01-22 09:35:36 +0800

    show_comments

--- src/main/java/com/example/zone/controller/DiscussPostController.java
+++ src/main/java/com/example/zone/controller/DiscussPostController.java
@@ -1,10 +1,14 @@
 package com.example.zone.controller;
 
+import com.example.zone.entity.Comment;
 import com.example.zone.entity.DiscussPost;
+import com.example.zone.entity.Page;
 import com.example.zone.entity.User;
+import com.example.zone.service.CommentService;
 import com.example.zone.service.DiscussPostService;
 import com.example.zone.service.UserService;
 import com.example.zone.utils.HostHolder;
+import com.example.zone.utils.ZoneConstant;
 import com.example.zone.utils.ZoneUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostController {
+public class DiscussPostController implements ZoneConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
@@ -28,6 +32,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -49,9 +56,21 @@ public class DiscussPostController {
     }
 
 
+//  未加评论
+//    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+//    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+//        // 帖子
+//        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+//        model.addAttribute("post", post);
+//        // 作者
+//        User user = userService.findUserById(post.getUserId());
+//        model.addAttribute("user", user);
+//
+//        return "site/discuss-detail";
+//    }
 
     @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page) {
         // 帖子
         DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
         model.addAttribute("post", post);
@@ -59,8 +78,65 @@ public class DiscussPostController {
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user", user);
 
-        return "site/discuss-detail";
+        // 评论分页信息
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        page.setRows(post.getCommentCount());
+
+        // 评论: 给帖子的评论
+        // 回复: 给评论的评论
+        // 评论列表
+        List<Comment> commentList = commentService.findCommentsByEntity(
+                ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimit());
+        // 评论VO列表
+        List<Map<String, Object>> commentVoList = new ArrayList<>();
+        if (commentList != null) {
+            for (Comment comment : commentList) {
+                // 评论VO
+                Map<String, Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment", comment);
+                // 作者
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+
+                // 回复列表
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE);
+                // 回复VO列表
+                List<Map<String, Object>> replyVoList = new ArrayList<>();
+                if (replyList != null) {
+                    for (Comment reply : replyList) {
+                        Map<String, Object> replyVo = new HashMap<>();
+                        // 回复
+                        replyVo.put("reply", reply);
+                        // 作者
+                        replyVo.put("user", userService.findUserById(reply.getUserId()));
+                        // 回复目标
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                        replyVo.put("target", target);
+
+                        replyVoList.add(replyVo);
+                    }
+                }
+                commentVo.put("replys", replyVoList);
+
+                // 回复数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+
+                commentVoList.add(commentVo);
+            }
+        }
+
+        model.addAttribute("comments", commentVoList);
+
+        return "/site/discuss-detail";
     }
+
+
+
+
+
 
 
 
--- src/main/java/com/example/zone/utils/ZoneConstant.java
+++ src/main/java/com/example/zone/utils/ZoneConstant.java
@@ -8,4 +8,7 @@ public  interface ZoneConstant   {
 
      int DEFAULT_EXPIRED_SECONDS=3600*12;
     int REMEMBER_EXPIRED_SECONDS=3600*24;
+
+    int ENTITY_TYPE_POST=1;
+    int ENTITY_TYPE_COMMENT=2;
 }
